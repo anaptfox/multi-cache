74bdcf9696e5663c87ea647a4c3a8a0fa9f6cd97
Taron Foxworth <taronfoxworth@gmail.com>
2014-02-02 02:13:16 -0500

    Fixed local storage

--- stores/localstorage.js
+++ stores/localstorage.js
@@ -1,123 +1,164 @@
 function now() {
+
   return (new Date).getTime();
+
 }
 
 function expired(record) {
+
   return record.expire && record.expire < now();
+
 }
 
-var Memory = function() {
+var LocalStorage = function() {
+
   var self = this;
+
   self.cache = {};
+
   self.debug = false;
+
   self.hitCount = 0;
+
   self.missCount = 0;
 
   return self;
 }
 
-Memory.prototype.put = function(key, value, time) {
+LocalStorage.prototype.put = function(key, value, time) {
+
+  var item = window.localstorage.getItem(key);
 
   // check localstorage for item and clear the timeout
-  if (window.localstorage.getItem(JSON.parse(key))) {
-    var item = window.localstorage.getItem(JSON.parse(key));
+  if (item) {
+
+    item = JSON.parse(item);
+
     clearTimeout(item.timeout);
+
   }
 
   var record = {
+
     value: value,
+
     expire: time ? (time + now()) : null
+
   };
 
   // set timeout for item to store in cache
   if (record.expire) {
+
     (function() {
+
       var timeout = setTimeout(function() {
-        window.localstorage.delItem(key);
+
+        _self.del(key);
+
       }, time);
+
       record.timeout = timeout;
+
     })();
+
   }
 
   // store record to key to store in cache
   window.localstorage.setItem(key, JSON.stringify(record)));
+
 }
 
 // delete item in local storage
-Memory.prototype.del = function(key) {
-  var item = window.localstorage.getItem(JSON.parse(key));
-  var record = item;
+LocalStorage.prototype.del = function(key) {
 
-  if (!record) {
+  var item = window.localstorage.getItem(key);
+
+  if (!item) {
+
     return false;
+
   }
+
+  var record = JSON.parse(item);
 
   clearTimeout(record.timeout);
 
   var isExpired = expired(record);
-  window.localstorage.delItem(key);
+
+  window.localstorage.removeItem(key);
+
   return !isExpired;
+
 }
 
-// need to be completed
-Memory.prototype.clear = function() {
-  var self = this;
+LocalStorage.prototype.clear = function() {
+  // TODO
+}
 
-  for (var key in self.cache) {
-    clearTimeout(self.cache[key].timeout);
+LocalStorage.prototype.get = function(key) {
+
+  var item = window.localstorage.getItem(key);
+
+  if (!item) {
+
+    return null;
+
   }
 
-  self.cache = {};
-}
+  var record = JSON.parse(item);
 
-Memory.prototype.get = function(key) {
-  var item = window.localstorage.getItem(JSON.parse(key));
-  var record = item;
   if (typeof record != "undefined") {
+
     if (!expired(record)) {
+
       item.debug && ++item.hitCount;
+
       return record.value;
+
     } else {
+
       item.debug && ++item.missCount;
-      window.localstorage.delItem(key);
+
+      window.localstorage.removeItem(key);
+
     }
+
   }
+
   return null;
+
 }
 
-// need to be re-evaluated
-Memory.prototype.memsize = function() {
+LocalStorage.prototype.memsize = function() {
+  // TODO
+}
+
+LocalStorage.prototype.hits = function() {
+
   var self = this;
-  var size = 0,
-    key;
-  for (key in self.cache) {
-    if (self.cache.hasOwnProperty(key)) {
-      size++;
-    }
-  }
-  return size;
+
+  return self.hitCount;
+
 }
 
-Memory.prototype.hits = function() {
-  var item = window.localstorage.getItem(JSON.parse(key));
-  return item.hitCount;
+LocalStorage.prototype.misses = function() {
+
+  var self = this;
+
+  return self.missCount;
+
 }
 
-Memory.prototype.misses = function() {
-  var item = window.localstorage.getItem(JSON.parse(key));
-  return item.missCount;
-}
-
-Memory.shared = new Memory();
+LocalStorage.shared = new LocalStorage();
 
 if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') {
-  module.exports = Memory;
+  module.exports = LocalStorage;
 } else {
   if (typeof(define) === 'function' && define.amd) {
     define([], function() {
-      return Memory;
+      return LocalStorage;
     });
   } else {
-    window.Memory = Memory;
+    window.LocalStorage = LocalStorage;
   }
 }
